d4c961cb30e568fccba0d8d3ec19d95499a9415a
giusmili <giusmili67@gmail.com>
2022-09-07 16:58:40 +0200

    first commit css

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", e => {
+     e.preventDefault();
+     /* 
+     DOM, les objets et propriétés, valeur par défaut 
+     */
+     const user = {
+          main_title: "Cours animation and js",
+          main_date_document: new Date(),
+          el: document.querySelector("title"),
+          footer: document.querySelector("footer p")
+     };
+     user.el.innerText = user.main_title;
+     user.footer.innerText += ` ${user.main_date_document.getFullYear()}`;
+     console.log(typeof user);
+     console.log(`${user.main_title} \n ${user.main_date_document}`);
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Cours animation CSS et javascript
+![logo html](https://i0.wp.com/byfeel.info/wp-content/uploads/2015/02/css-html2-e1517475681211.png?fit=400%2C259&ssl=1)
+> &copy; Giuseppe Militello - All rights reserved for educational purposes only 
